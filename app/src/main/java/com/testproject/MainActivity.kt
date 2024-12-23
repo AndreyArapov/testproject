2370a6e66078ffd287d7ae49fa1ce4f3ed67762d
Andrey Arapov <aarapov@avito.ru>
2024-12-23 15:07:20 +0500

    init project

---
+++ app/src/main/java/com/testproject/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.testproject
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.testproject.ui.theme.TestProjectTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TestProjectTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Column(
+                        modifier = Modifier
+                            .padding(innerPadding)
+                            .fillMaxSize()
+                    ) {
+                        Text(stringResource(R.string.feature_1))
+                        Text(stringResource(com.testproject.feature.R.string.feature_2))
+                    }
+                }
+            }
+        }
+    }
+}
